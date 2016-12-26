de7aa30547d215f2322b309a1523b5b182268228
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2016-12-26 22:06:18 +0800

    将组建导出到dll

--- COM/RawCom/InterfaceImpl.cpp
+++ COM/RawCom/InterfaceImpl.cpp
@@ -1,33 +1,20 @@
-#include"stdafx.h"
-#include"DictionaryImp.h"
+#include "stdafx.h"
+#include"InterfaceDecl.h"
 
-//{00000000-0000-0000-C000-000000000046} 统一的IID_IUnknown,所有COM组件的IID_IUnknown都相同
-static const IID IID_IUnknown = {
-	0x00000000, 0x0000, 0x0000,{ 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46 }
-};
-
-// {2B7846C4-2005-47D9-BFC4-6E9DBF3730D9}
-static const IID IID_IDictionary = {
- 0x2b7846c4, 0x2005, 0x47d9,{ 0xbf, 0xc4, 0x6e, 0x9d, 0xbf, 0x37, 0x30, 0xd9 }
-};
-
-// {687C9920-A4DB-4A57-9126-C5F4044D8704}
-static const IID IID_ISpellCheck ={
-	0x687c9920, 0xa4db, 0x4a57,{ 0x91, 0x26, 0xc5, 0xf4, 0x4, 0x4d, 0x87, 0x4 }
-};
-
-
-CDictionary::CDictionary(){}
-CDictionary::~CDictionary(){}
+CDictionary::CDictionary():m_ref(0){}
+CDictionary::~CDictionary() {}
 
 HRESULT _stdcall CDictionary::QueryIntertface(const IID& iid, void** ppv) {
 	if (IID_IUnknown == iid) {
 		*ppv = static_cast<IDictionary*>(this);
-	}else if (IID_IDictionary == iid) {
+	}
+	else if (IID_IDictionary == iid) {
 		*ppv = static_cast<IDictionary*>(this);
-	}else if (IID_ISpellCheck == iid) {
+	}
+	else if (IID_ISpellCheck == iid) {
 		*ppv = static_cast<ISpellCheck*>(this);
-	}else {
+	}
+	else {
 		*ppv = NULL;
 		return E_NOINTERFACE;
 	}
@@ -36,26 +23,36 @@ HRESULT _stdcall CDictionary::QueryIntertface(const IID& iid, void** ppv) {
 }
 
 ULONG _stdcall CDictionary::AddRef() {
-	return 0;
+	std::cout << "CDictionary::AddRef" << std::endl;
+	return ++m_ref;
 }
 
 ULONG _stdcall CDictionary::Releace() {
-	return 0;
+	std::cout << "CDictionary::Releace" << std::endl;
+	if (--m_ref == 0) {
+		delete this;
+		std::cout << "Destory CDictionary" << std::endl;
+	}
+	return m_ref;
 }
 
 bool _stdcall CDictionary::Init() {
+	std::cout << "CDictionary::Init" << std::endl;
 	return true;
 }
 
 bool _stdcall CDictionary::LookupWord(std::string word) {
+	std::cout << "CDictionary::LookupWord : " << word << std::endl;
 	return true;
 }
 
 bool _stdcall CDictionary::UnInit() {
+	std::cout << "CDictionary::UnInit" << std::endl;
 	return true;
 }
 
 bool _stdcall CDictionary::CheckSpell(std::string word) {
+	std::cout << "CDictionary::CheckSpell : " << word << std::endl;
 	return true;
 }
 
