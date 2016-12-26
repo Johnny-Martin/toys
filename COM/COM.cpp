96c7e7da57c39f546fefac20fd2ef9b7baa48e1d
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2016-12-26 22:09:30 +0800

    COM主程序中模拟调用 dll中的组件

--- COM/COM.cpp
+++ COM/COM.cpp
@@ -1,25 +1,71 @@
 // COM.cpp : 定义控制台应用程序的入口点。
 //
-
 #include "stdafx.h"
+#include "InterfaceDecl.h"  
 
-//widget.dll
-
-#include "atlcomcli.h"  
-#import "Debug\\ATLProject.dll" no_namespace  
-
+IUnKnown* CallCreateInstance(TCHAR* name) {
+	HINSTANCE hInst = ::LoadLibrary(name);
+	if (NULL == hInst) {
+		std::cout << "NULL == hInst";
+		return NULL;
+	}
+	typedef IUnKnown* (*CreateInstanceFunType)();
+	CreateInstanceFunType CreateInstanceFun = (CreateInstanceFunType)::GetProcAddress(hInst, "CreateInstance");
+	if (NULL == CreateInstanceFun) {
+		std::cout << "NULL == CreateInstanceFun";
+		return NULL;
+	}
+	return CreateInstanceFun();
+}
 int main()
 {
-	CoInitialize(NULL);
-	CLSID clsid;
-	CLSIDFromProgID(OLESTR("SmpMath.1"), &clsid);
-	CComPtr<ISmpMath> pFirstClass;//智能指针  
-	pFirstClass.CoCreateInstance(clsid);
-	long ret = pFirstClass->Add(1, 2);
-	printf("%d\n", ret);
-	pFirstClass.Release();
-	CoUninitialize();
+	//CoInitialize(NULL);
+	//CLSID clsid;
+	//CLSIDFromProgID(OLESTR("SmpMath.1"), &clsid);
+	//CComPtr<ISmpMath> pFirstClass;//智能指针  
+	//pFirstClass.CoCreateInstance(clsid);
+	//long ret = pFirstClass->Add(1, 2);
+	//printf("%d\n", ret);
+	//pFirstClass.Release();
+	//CoUninitialize();
 
+	IUnKnown* pIUnknown = CallCreateInstance(_T("simulation2.dll"));
+	if (NULL == pIUnknown) {
+		std::cout << "NULL == pIUnknown";
+		return -1;
+	}
+
+	IDictionary* pIDictionary;
+	ISpellCheck* pISpellCheck;
+	HRESULT hr = pIUnknown->QueryIntertface(IID_IDictionary, (void**)&pIDictionary);
+	if (SUCCEEDED(hr)) {
+		pIDictionary->Init();
+		pIDictionary->LookupWord("hello");
+		pIDictionary->UnInit();
+
+		//使用pIDictionary 获得 pISpellCheck
+		hr = pIDictionary->QueryIntertface(IID_ISpellCheck, (void**)&pISpellCheck);
+		if (SUCCEEDED(hr)) {
+			std::cout << pISpellCheck << std::endl;
+			pISpellCheck->CheckSpell("fuck");
+			pISpellCheck->Releace();
+			pISpellCheck = NULL;
+		}
+
+		pIDictionary->Releace();
+		pIDictionary = NULL;
+	}
+
+	//使用pIUnknowm 获得 pISpellCheck
+	hr = pIUnknown->QueryIntertface(IID_ISpellCheck, (void**)&pISpellCheck);
+	if (SUCCEEDED(hr)) {
+		std::cout << pISpellCheck << std::endl;
+		pISpellCheck->CheckSpell("fuck");
+		pISpellCheck->Releace();
+		pISpellCheck = NULL;
+	}
+
+	pIUnknown->Releace();
 	return 0;
 }
 
