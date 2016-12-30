a182840dcd5041f29436a7b3baf66eee27ac460a
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2016-12-30 21:50:26 +0800

    simulation3

---
+++ COM/simulation3/InterfaceDecl.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "stdafx.h"
+
+class IUnKnown {
+public:
+	virtual HRESULT _stdcall QueryIntertface(const IID& iid, void** ppv) = 0;
+	virtual ULONG   _stdcall AddRef() = 0;
+	virtual ULONG   _stdcall Releace() = 0;
+};
+
+class IDictionary : public IUnKnown {
+public:
+	virtual bool _stdcall Init() = 0;
+	virtual bool _stdcall LookupWord(std::string word) = 0;
+	virtual bool _stdcall UnInit() = 0;
+};
+
+class ISpellCheck : public IUnKnown {
+public:
+	virtual bool _stdcall CheckSpell(std::string word) = 0;
+};
+
+class CDictionary :public IDictionary,
+	public ISpellCheck
+{
+public:
+	CDictionary();
+	~CDictionary();
+public:
+	virtual HRESULT _stdcall QueryIntertface(const IID& iid, void** ppv);
+	virtual ULONG   _stdcall AddRef();
+	virtual ULONG   _stdcall Releace();
+	virtual bool    _stdcall Init(void);
+	virtual bool    _stdcall LookupWord(std::string word);
+	virtual bool    _stdcall UnInit(void);
+	virtual bool    _stdcall CheckSpell(std::string word);
+private:
+	unsigned long m_ref;
+};
+
+IUnKnown* CreateInstance(void);
+extern"C" HRESULT __stdcall DLLRegisterServer();
+extern"C" HRESULT __stdcall DLLUnregisterServer();
+
+extern const IID IID_IUnknown;
+extern const IID IID_IDictionary;
+extern const IID IID_ISpellCheck;
--- COM/simulation3/InterfaceImpl.cpp
+++ COM/simulation3/InterfaceImpl.cpp
@@ -1,40 +1,85 @@
-// COM.cpp : 定义控制台应用程序的入口点。
-//
 #include "stdafx.h"
-#include "InterfaceDecl.h"  
+#include"InterfaceDecl.h"
 
-IUnKnown* CallCreateInstance(TCHAR* name) {
-	HINSTANCE hInst = ::LoadLibrary(name);
-	if (NULL == hInst) {
-		std::cout << "NULL == hInst";
-		return NULL;
+CDictionary::CDictionary():m_ref(0){}
+CDictionary::~CDictionary() {}
+
+HRESULT _stdcall CDictionary::QueryIntertface(const IID& iid, void** ppv) {
+	if (IID_IUnknown == iid) {
+		*ppv = static_cast<IDictionary*>(this);
 	}
-	typedef IUnKnown* (*CreateInstanceFunType)();
-	CreateInstanceFunType CreateInstanceFun = (CreateInstanceFunType)::GetProcAddress(hInst, "CreateInstance");
-	if (NULL == CreateInstanceFun) {
-		std::cout << "NULL == CreateInstanceFun";
-		return NULL;
+	else if (IID_IDictionary == iid) {
+		*ppv = static_cast<IDictionary*>(this);
 	}
-	return CreateInstanceFun();
+	else if (IID_ISpellCheck == iid) {
+		*ppv = static_cast<ISpellCheck*>(this);
+	}
+	else {
+		*ppv = NULL;
+		return E_NOINTERFACE;
+	}
+	reinterpret_cast<IUnKnown*>(*ppv)->AddRef();
+	return S_OK;
 }
+
+ULONG _stdcall CDictionary::AddRef() {
+	std::cout << "CDictionary::AddRef" << std::endl;
+	return ++m_ref;
+}
+
+ULONG _stdcall CDictionary::Releace() {
+	std::cout << "CDictionary::Releace" << std::endl;
+	if (--m_ref == 0) {
+		delete this;
+		std::cout << "Destory CDictionary" << std::endl;
+	}
+	return m_ref;
+}
+
+bool _stdcall CDictionary::Init() {
+	std::cout << "CDictionary::Init" << std::endl;
+	return true;
+}
+
+bool _stdcall CDictionary::LookupWord(std::string word) {
+	std::cout << "CDictionary::LookupWord : " << word << std::endl;
+	return true;
+}
+
+bool _stdcall CDictionary::UnInit() {
+	std::cout << "CDictionary::UnInit" << std::endl;
+	return true;
+}
+
+bool _stdcall CDictionary::CheckSpell(std::string word) {
+	std::cout << "CDictionary::CheckSpell : " << word << std::endl;
+	return true;
+}
+
+IUnKnown* CreateInstance() {
+	IUnKnown* pRet = static_cast<IDictionary*>(new CDictionary);
+	pRet->AddRef();
+	return pRet;
+}
+
 
 HRESULT __stdcall DLLRegisterServer() {
 	/*HKEY_CLASSES_ROOT
-	|--CLSID
-	|--{354CD5C5-839B-4A1E-8033-0EBC5246A4EF}--SimulationComObject
-	|--InprocServer32    --C:\XXX\SSS\simulation3.dll
-	|--ProgID            --COM.Simulation.1
-	...
-	|--COM.Simulation
-	|--CLSID--{354CD5C5-839B-4A1E-8033-0EBC5246A4EF}
-	|--CurVer--COM.Simulation.1
+			|--CLSID
+			     |--{354CD5C5-839B-4A1E-8033-0EBC5246A4EF}--SimulationComObject
+				                     |--InprocServer32    --C:\XXX\SSS\simulation3.dll
+									 |--ProgID            --COM.Simulation.1
+		   ...
+			|--COM.Simulation
+			     |--CLSID--{354CD5C5-839B-4A1E-8033-0EBC5246A4EF}
+				 |--CurVer--COM.Simulation.1
 
 	*/
 
 
 	// {354CD5C5-839B-4A1E-8033-0EBC5246A4EF}
 	//static const GUID ComponentCLSID = { 0x354cd5c5, 0x839b, 0x4a1e,{ 0x80, 0x33, 0xe, 0xbc, 0x52, 0x46, 0xa4, 0xef } };
-
+	
 	wchar_t wszCLSID[] = _T("{354cd5c5-839b-4a1e-8033-0ebc5246ef}");
 	//::StringFromGUID2(ComponentCLSID, wszCLSID, 39);
 
@@ -49,7 +94,7 @@ HRESULT __stdcall DLLRegisterServer() {
 	//	std::cout << "写注册表失败" << std::endl;
 	//	return;
 	//}
-
+	
 
 	wchar_t wszComponentDesc[] = _T("SimulationComObject");
 	length = sizeof(wszComponentDesc) / sizeof(wchar_t);
@@ -61,9 +106,9 @@ HRESULT __stdcall DLLRegisterServer() {
 	}
 	RegCloseKey(Key);
 
-	wchar_t wszModulePath[MAX_PATH] = { 0 };
+	wchar_t wszModulePath[MAX_PATH] = {0};
 	GetModuleFileName(NULL, wszModulePath, MAX_PATH);
-	length = wcslen(wszModulePath);
+	length =wcslen(wszModulePath);
 	RegCreateKey(HKEY_CLASSES_ROOT, _T("CLSID//{354cd5c5-839b-4a1e-8033-0ebc5246ef}//InprocServer32"), &Key);
 	retValue = RegSetValue(Key, _T(""), REG_SZ, wszModulePath, length);
 	if (ERROR_SUCCESS != retValue) {
@@ -103,57 +148,8 @@ HRESULT __stdcall DLLRegisterServer() {
 	return S_OK;
 }
 
-int main()
-{
-	//CoInitialize(NULL);
-	//CLSID clsid;
-	//CLSIDFromProgID(OLESTR("SmpMath.1"), &clsid);
-	//CComPtr<ISmpMath> pFirstClass;//智能指针  
-	//pFirstClass.CoCreateInstance(clsid);
-	//long ret = pFirstClass->Add(1, 2);
-	//printf("%d\n", ret);
-	//pFirstClass.Release();
-	//CoUninitialize();
+HRESULT __stdcall DLLUnregisterServer() {
 
-	DLLRegisterServer();
-
-	IUnKnown* pIUnknown = CallCreateInstance(_T("simulation2.dll"));
-	if (NULL == pIUnknown) {
-		std::cout << "NULL == pIUnknown";
-		return -1;
-	}
-
-	IDictionary* pIDictionary;
-	ISpellCheck* pISpellCheck;
-	HRESULT hr = pIUnknown->QueryIntertface(IID_IDictionary, (void**)&pIDictionary);
-	if (SUCCEEDED(hr)) {
-		pIDictionary->Init();
-		pIDictionary->LookupWord("hello");
-		pIDictionary->UnInit();
-
-		//使用pIDictionary 获得 pISpellCheck
-		hr = pIDictionary->QueryIntertface(IID_ISpellCheck, (void**)&pISpellCheck);
-		if (SUCCEEDED(hr)) {
-			std::cout << pISpellCheck << std::endl;
-			pISpellCheck->CheckSpell("fuck");
-			pISpellCheck->Releace();
-			pISpellCheck = NULL;
-		}
-
-		pIDictionary->Releace();
-		pIDictionary = NULL;
-	}
-
-	//使用pIUnknowm 获得 pISpellCheck
-	hr = pIUnknown->QueryIntertface(IID_ISpellCheck, (void**)&pISpellCheck);
-	if (SUCCEEDED(hr)) {
-		std::cout << pISpellCheck << std::endl;
-		pISpellCheck->CheckSpell("fuck");
-		pISpellCheck->Releace();
-		pISpellCheck = NULL;
-	}
-
-	pIUnknown->Releace();
-	return 0;
+	return S_OK;
 }
 
