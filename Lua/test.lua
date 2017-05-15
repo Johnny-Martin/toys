9541c2bcdc5b06d493f09b988a1a9c32fc6d24cf
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-15 17:52:43 +0800

    require

--- Lua/test.lua
+++ Lua/test.lua
@@ -6,3 +6,13 @@ if LibDemo then
 else
 	print(msg)
 end
+
+local luacom = require("luacom")
+if luacom then
+	local comObj = luacom.CreateObject("SmpMath.1")
+	if comObj then
+		print("Add: 8 + 9: "..tostring(comObj:Add(8 + 9)))
+	end
+else
+	print("can not open luacom")
+end
