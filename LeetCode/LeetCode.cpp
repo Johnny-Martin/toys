3762b02adf2c1a09c683ce030e9f8ef689a62b4d
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-15 18:19:21 +0800

    build

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
-
+#include "Solutions\TwoSum.h"
+#include "Solutions\TriSum.h"
+#include "Solutions\AddTwoNumbers.h"
+#include "Solutions\LongestSubString.h"
 
 int main()
 {
+	vector<int> test = { 0,3,0,1,1,-1,-5,-5,3,-3,-3,0 };
+	TriSumSolution sul;
+
+	vector<vector<int>> ret = sul.threeSum(test);
+
+	AddTwoNumsSolution AddTwoNumsSolutionTest;
+	AddTwoNumsSolutionTest.test();
+
+	LongestSubStringSolution LongestSubStringSolutionTGest;
+	LongestSubStringSolutionTGest.test();
     return 0;
 }
 
