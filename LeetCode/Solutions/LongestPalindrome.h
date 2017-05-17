d3186fb54ec7ff227bf4375cf4fbbd72bfe99ef1
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-17 16:50:32 +0800

    从中间向两侧查找回文串，accepted

--- LeetCode/Solutions/LongestPalindrome.h
+++ LeetCode/Solutions/LongestPalindrome.h
@@ -5,41 +5,43 @@
 #include <iostream>
 using namespace std;
 
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-
 class LongestPalindromeSolution {
-	int IsPalindrome(string s, unsigned  int iBegin, unsigned  int iEnd) {
-		int sum = iBegin + iEnd;
-		for (unsigned  int i = iBegin; i <= (iEnd + iBegin)/2; ++i) {
-			if (s[i] != s[sum - i])
-				return 0;
-		}
-		return iEnd - iBegin + 1;
-	}
+
 public:
 	string longestPalindrome(string s) {
-		unsigned int length = 0;
-		unsigned  int iBegin = 0;
-		unsigned  int iEnd = 0;
+		unsigned int maxLength = 0;
+		unsigned  int iLeft = 0;
 
-		for(unsigned  int i=0; i<s.length(); ++i){
-			for (unsigned  int j = s.length() - 1; j >= MAX(i, iEnd); --j) {
-				unsigned  int tmp = IsPalindrome(s, i, j);
-				if (tmp > length) {
-					length = tmp;
-					iBegin = i;
-					iEnd = j;
-					break;
-				}else if (tmp > 0) {
+		for(int i=0; i<s.length(); ++i){
+			int iBegin = i;
+			int iEnd = i;
+			int length = 0;
+			while (s[iEnd] == s[iEnd + 1]) ++iEnd;
+			length = iEnd - iBegin + 1;
+			if (length > maxLength) {
+				maxLength = length;
+				iLeft = iBegin;
+			}
+
+			while (--iBegin >= 0 && ++iEnd < s.length()) {
+				if (s[iBegin] == s[iEnd]) {
+					length = iEnd - iBegin + 1;
+					if (length > maxLength) {
+						maxLength = length;
+						iLeft = iBegin;
+					}
+				}else {
 					break;
 				}
 			}
+			
 		}
-		return s.substr(iBegin, iEnd - iBegin + 1);
+		return s.substr(iLeft, maxLength);
 	}
 
 	void test() {
-		string ret = longestPalindrome("bacbbcad");
+		string ret = longestPalindrome("zbbb");
+		ret = longestPalindrome("bacbbcad");
 		ret = longestPalindrome("bb");
 		ret = longestPalindrome("babad");
 		ret = longestPalindrome("a");
