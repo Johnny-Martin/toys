aefc02c74a5a48f137e40f3fbd78128d2f8b1487
majianting <2777879315@qq.com>
2018-07-31 10:30:54 +0800

    update

--- MultiThread/Producer.h
+++ MultiThread/Producer.h
@@ -12,11 +12,11 @@ extern HANDLE hMutex;
 extern int iRes;
 
 DWORD WINAPI Producer(LPVOID) {
-	int count = 5;//让生产者工作5次后结束
+	int count = 50;//让生产者工作5次后结束
 	while (count-- > 0) {
 		WaitForSingleObject(hEmpty, INFINITE);
 		WaitForSingleObject(hMutex, INFINITE);
-		atomcout.out("生产一次, ", ++iRes,"\n");
+		atomcout.out("Produc one, ", ++iRes,"\n");
 		INFO("Producer a:, b: ");
 		ReleaseMutex(hMutex);
 		ReleaseSemaphore(hFull, 1, NULL);
@@ -25,11 +25,11 @@ DWORD WINAPI Producer(LPVOID) {
 }
 
 DWORD WINAPI Customer(LPVOID) {
-	int count = 5;//让生产者工作5次后结束
+	int count = 50;//让生产者工作5次后结束
 	while (count-- > 0) {
 		WaitForSingleObject(hFull, INFINITE);
 		WaitForSingleObject(hMutex, INFINITE);
-		atomcout.out( "消费一次, ", --iRes, "\n");
+		atomcout.out( "Custom one, ", --iRes, "\n");
 		INFO("Customer a:, b: ");
 		ReleaseMutex(hMutex);
 		ReleaseSemaphore(hEmpty, 1, NULL);
